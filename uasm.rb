8085369149aadcb5de78a9e23216b0f516f42f66
unknown <john@terraspace.co.uk>
2018-11-07 15:47:51 +0000

    remove bottle

--- uasm.rb
+++ uasm.rb
@@ -4,7 +4,6 @@ class Uasm < Formula
   url "http://www.terraspace.co.uk/uasm_osx.zip", :using => :curl
   version "2.46"
   sha256 "9621c796a51b355f7a8420f6cf1d49ef23d27ae7b3bd9e7441a3a819e8df8d3e"
-  bottle :unneeded
   
   def install
     bin.install "uasm"
