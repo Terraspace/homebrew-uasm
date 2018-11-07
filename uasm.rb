edeac6dd6ca3234ff6da3c762896a50071e1f7fd
unknown <john@terraspace.co.uk>
2018-11-07 16:15:42 +0000

    remove bottle

--- uasm.rb
+++ uasm.rb
@@ -1,7 +1,7 @@
 class Uasm < Formula
   desc "UASM - MASM compatible assembler"
   homepage "https://github.com/Terraspace/UASM"
-  url "http://www.terraspace.co.uk/uasm_osx.zip"
+  url "http://www.terraspace.co.uk/uasm-2.47.zip"
   sha256 "9621c796a51b355f7a8420f6cf1d49ef23d27ae7b3bd9e7441a3a819e8df8d3e"
 
   bottle do
