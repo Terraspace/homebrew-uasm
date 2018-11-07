85fbecd91e2b90decc4dd8f7c64b54fb875588b8
unknown <john@terraspace.co.uk>
2018-11-07 14:59:32 +0000

    Add formula SHA256

--- uasm.rb
+++ uasm.rb
@@ -1,11 +1,12 @@
 class Uasm < Formula
   desc "UASM - MASM compatible assembler"
   homepage "https://github.com/Terraspace/UASM"
-
-
   url "http://www.terraspace.co.uk/uasm_osx.zip", :using => :curl
-
+  version "2.46"
+  sha256 "9621C796A51B355F7A8420F6CF1D49EF23D27AE7B3BD9E7441A3A819E8DF8D3E"
+  bottle: unneeded
+  
   def install
-    bin.install "terraspace/uasm"
+    bin.install "uasm"
   end
 end
