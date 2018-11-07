a09207e68637d801780d2bafecbf9ba82bc36dd2
unknown <john@terraspace.co.uk>
2018-11-07 15:05:32 +0000

    Add formula

--- uasm.rb
+++ uasm.rb
@@ -4,7 +4,7 @@ class Uasm < Formula
   url "http://www.terraspace.co.uk/uasm_osx.zip", :using => :curl
   version "2.46"
   sha256 "9621C796A51B355F7A8420F6CF1D49EF23D27AE7B3BD9E7441A3A819E8DF8D3E"
-  bottle: unneeded
+  bottle :unneeded
   
   def install
     bin.install "uasm"
